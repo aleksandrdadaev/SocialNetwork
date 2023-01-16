0710d4b121c19b4683e7745e263e587e29479d30
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-01-16 16:24:52 +0700

    Накидал базовую структуру страницы Профиль

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<header>Logo</header>
+			<nav>
+				<span>Profile</span>
+				<span>Messages</span>
+				<span>News</span>
+				<span>Music</span>
+				<span>Settings</span>
+			</nav>
+			<section>
+				<span>Cover</span>
+				<span>Photo</span>
+				<span>Information</span>
+			</section>
+		</div>
+	);
 }
 
 export default App;
