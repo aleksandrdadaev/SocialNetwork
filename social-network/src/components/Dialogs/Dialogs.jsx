c6893514e157bd73537887361fcff1ac4f4d6948
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-01-19 13:03:40 +0700

    Общая стилизация страницы Сообщения

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import DialogsList from './DialogsList/DialogsList';
+import Search from './Search/Search';
 
 const Dialogs = props => {
-	return <section>Dialogs </section>;
+	return (
+		<section className={s.dialogs}>
+			<div className={s.leftPanel}>
+				<Search />
+				<DialogsList />
+			</div>
+			<div className={s.rightPanel}></div>
+		</section>
+	);
 };
 
 export default Dialogs;
---
+++ social-network/src/components/Dialogs/DialogsList/DialogsList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './DialogsList.module.css';
+
+const DialogsList = props => {
+	return <div className={s.dialogsList}></div>;
+};
+
+export default DialogsList;
