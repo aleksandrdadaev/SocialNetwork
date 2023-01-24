0745996b93e484d34e17c7090c13af5f0256d0db
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-01-24 13:18:03 +0700

    Собрали state и функция усправления им в один объект store

--- social-network/src/index.js
+++ social-network/src/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
+import store from './redux/state';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostInputValue, subscribe } from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let render = () => {
+let rerender = state => {
 	root.render(
 		<React.StrictMode>
 			<BrowserRouter>
 				<App
 					state={state}
-					addPost={addPost}
-					updateNewPostInputValue={updateNewPostInputValue}
+					addPost={store.addPost.bind(store)}
+					updateNewPostInputValue={store.updateNewPostInputValue.bind(store)}
 				/>
 			</BrowserRouter>
 		</React.StrictMode>
 	);
 };
 
-render();
+rerender(store.getState());
 
-subscribe(render);
+store.subscribe(rerender);
 
 reportWebVitals();
--- social-network/src/redux/state.js
+++ social-network/src/redux/state.js
@@ -1,50 +1,51 @@
-let render = () => {
-	console.log('State changed');
-};
-
-let state = {
-	profilePage: {
-		posts: [
-			{ id: 1, text: 'Hi, how are you?' },
-			{ id: 2, text: "It's my second post" },
-		],
-		newPostInputValue: '',
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 1, text: 'Hi, how are you?' },
+				{ id: 2, text: "It's my second post" },
+			],
+			newPostInputValue: '',
+		},
+		dialogsPage: {
+			dialogs: [
+				{ id: 1, name: 'Sanchezz' },
+				{ id: 2, name: 'Danil' },
+				{ id: 3, name: 'Pahan' },
+			],
+			messages: [
+				{ id: 1, text: 'Hi' },
+				{
+					id: 2,
+					text: 'How are you?',
+				},
+				{ id: 3, text: 'Hi' },
+				{ id: 4, text: 'Yo' },
+			],
+		},
 	},
-	dialogsPage: {
-		dialogs: [
-			{ id: 1, name: 'Sanchezz' },
-			{ id: 2, name: 'Danil' },
-			{ id: 3, name: 'Pahan' },
-		],
-		messages: [
-			{ id: 1, text: 'Hi' },
-			{
-				id: 2,
-				text: 'How are you?',
-			},
-			{ id: 3, text: 'Hi' },
-			{ id: 4, text: 'Yo' },
-		],
+	getState() {
+		return this._state;
+	},
+	_callSubscriber() {
+		console.log('State changed');
+	},
+	addPost() {
+		let newPost = {
+			id: 5,
+			text: this._state.profilePage.newPostInputValue,
+		};
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostInputValue = '';
+		this._callSubscriber(this._state);
+	},
+	updateNewPostInputValue(newValue) {
+		this._state.profilePage.newPostInputValue = newValue;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer; // наблюдатель
 	},
 };
 
-export const addPost = () => {
-	let newPost = {
-		id: 5,
-		text: state.profilePage.newPostInputValue,
-	};
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostInputValue = '';
-	render();
-};
-
-export const updateNewPostInputValue = newText => {
-	state.profilePage.newPostInputValue = newText;
-	render();
-};
-
-export const subscribe = observer => {
-	render = observer; // наблюдатель
-};
-
-export default state;
+export default store;
