5ef57def81d2c83141eef6ea207571833cf1cdb2
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-02-10 13:45:12 +0700

    Познакомился с локальным стейтом у классовых компонент, добавилтакой стейт к статусу на странице профиля

--- social-network/src/components/Profile/ProfileContainer.jsx
+++ social-network/src/components/Profile/ProfileContainer.jsx
@@ -25,7 +25,7 @@ let mapStateToProps = state => {
 };
 
 export default compose(
-	withAuthRedirect,
+	// withAuthRedirect,
 	withRouter,
 	connect(mapStateToProps, { getProfile })
 )(ProfileContainer);
