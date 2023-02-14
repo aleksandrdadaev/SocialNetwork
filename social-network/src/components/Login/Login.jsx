badac0ea821539998f45034423ba207aa0d8a19f
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-02-14 11:51:45 +0700

    Добавил форму входа на страницу /login

--- social-network/src/components/Login/Login.jsx
+++ social-network/src/components/Login/Login.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import styles from './Login.module.css';
 
 const Login = props => {
-	return <section>Login</section>;
+	return (
+		<section className={styles.blok}>
+			<LoginForm />
+		</section>
+	);
+};
+
+const LoginForm = props => {
+	return (
+		<form className={styles.form}>
+			<input type='text' placeholder='Login' className={styles.input} />
+			<input type='text' placeholder='Password' className={styles.input} />
+			<label className={styles.label}>
+				<input type='checkbox'></input>
+				<span className={styles.labelText}>remember me</span>
+			</label>
+			<button className={styles.button}>Login</button>
+		</form>
+	);
 };
 
 export default Login;
