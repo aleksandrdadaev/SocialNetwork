845dc7ba40a40bae7dd795fbf2e9366a461788df
Aleksandr Dadaev <sasha.dadaev.98@mail.ru>
2023-02-14 16:51:00 +0700

    Подключл библиотеку redux-form (Устаревшая), переделал формму логину с этой библиотекой

--- social-network/src/components/Login/Login.jsx
+++ social-network/src/components/Login/Login.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 import styles from './Login.module.css';
 
 const Login = props => {
+	const onSubmit = formData => {
+		console.log(formData);
+	};
 	return (
 		<section className={styles.blok}>
-			<LoginForm />
+			<LoginReduxForm onSubmit={onSubmit} />
 		</section>
 	);
 };
 
 const LoginForm = props => {
 	return (
-		<form className={styles.form}>
-			<input type='text' placeholder='Login' className={styles.input} />
-			<input type='text' placeholder='Password' className={styles.input} />
+		<form className={styles.form} onSubmit={props.handleSubmit}>
+			<Field
+				type='text'
+				placeholder='Login'
+				className={styles.input}
+				component='input'
+				name='login'
+			/>
+			<Field
+				type='password'
+				placeholder='Password'
+				className={styles.input}
+				component='input'
+				name='password'
+			/>
 			<label className={styles.label}>
-				<input type='checkbox'></input>
+				<Field component='input' type='checkbox' name='rememberMe' />
 				<span className={styles.labelText}>remember me</span>
 			</label>
 			<button className={styles.button}>Login</button>
 		</form>
 	);
 };
+
+const LoginReduxForm = reduxForm({
+	form: 'login',
+})(LoginForm);
 
 export default Login;
